94e1ac02b32995b742185bceca62442e72ee9594
USER-ACER-PC <nicothecruzguardian@gmail.com>
2016-10-05 14:41:20 +0700

    First Love Coding "Nico Ardian Nugroho Part 1"

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/appcode/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/appcode/MainActivity.java
@@ -1,5 +1,6 @@
 package id.sch.smktelkom_mlg.tugas01.xirpl2019.appcode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button home = (Button) findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(getApplicationContext(),home.class);
+
+                startActivity(i);
+            }
+        });
         Button exit = (Button) findViewById(R.id.exit);
         exit.setOnClickListener(new View.OnClickListener() {
 
