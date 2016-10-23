1ca9266f55c40f55b4b7e41f6d18caf8b95033c3
Aisyatush Shofiah <aisyatushshofiah@gmail.com>
2016-10-23 16:45:48 +0700

    Second Love Coding "Aisyatush Shofiah Part2"

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/appcode/TwoFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/appcode/TwoFragment.java
@@ -1,7 +1,27 @@
 package id.sch.smktelkom_mlg.tugas01.xirpl2019.appcode;
 
-/**
- * Created by MS on 23/10/2016.
- */
-public class TwoFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TwoFragment extends Fragment {
+
+    public TwoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
 }
